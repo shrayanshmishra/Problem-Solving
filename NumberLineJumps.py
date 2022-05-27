e9c28948d5002a0accc9abbfafbb4daa2f96f514
Shrayansh Mishra <shreyanshmishra477@gmail.com>
2022-05-27 17:48:45 +0530

    Create NumberLineJumps.py
    A kangaroo starts at location l1 and moves at a rate of x meters per jump. The second kangaroo starts at another location l2 and moves at a rate of y meters per jump. Determine whether they meet or nor.

---
+++ NumberLineJumps.py
@@ -0,0 +1,37 @@
+x1 = 21
+x2 = 47
+v1 = 6
+v2 = 3
+def kangaroo(x1, v1, x2, v2):
+    if x1 < x2 and v1 < v2:
+        return "NO"
+    elif x1 > x2 and v1 > v2:
+        return "NO"
+    elif x1 == x2 and v1 == v2:
+        return "YES"
+    elif x1 != x2 and v1 == v2:
+            return "NO"
+    elif x1 == x2 and v1 != v2:
+        return "NO"
+    elif x1 < x2 and v1 > v2:
+        i = 1
+        while True:
+            if x1 > x2:
+                return "NO"
+            elif x1 == x2:
+                return "YES"
+            else:
+                x1 += v1 * i
+                x2 += v2 * i
+    elif x1 > x2 and v1 < v2:
+        i = 1
+        while True:
+            if x1 < x2:
+                return "NO"
+            elif x1 == x2:
+                return "YES"
+            else:
+                x1 += v1 * i
+                x2 += v2 * i
+res = kangaroo(x1, v1, x2, v2)
+print(res)
