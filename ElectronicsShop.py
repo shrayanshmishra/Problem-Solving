6cc3a6508ca609839350d1ae779de8d8d0b87fc3
Shrayansh Mishra <shreyanshmishra477@gmail.com>
2022-05-27 17:49:34 +0530

    Create ElectronicsShop.py
    In this problem you have to find the max. amount a certain man can spend keeping in mind it is not more that the man's budget. The man needs to buy a keyboard and a drive, if he can't buy both he won't spend any.

---
+++ ElectronicsShop.py
@@ -0,0 +1,22 @@
+budget = 374625
+keyboardVar = 797
+usbVar =  951
+keyboards = list(map(int, input().rstrip().split())) # [40, 50, 60]
+usbs = list(map(int, input().rstrip().split())) # [5, 8, 12]
+
+maxBuy = 0
+buyChkAr = []
+
+for kb in keyboards:
+    for usb in usbs:
+        if kb < budget and usb < budget:
+            if kb + usb <= budget:
+                maxBuy = kb + usb
+                buyChkAr.append(maxBuy)
+
+# print(buyChkAr)
+
+if buyChkAr:            
+    print(max(buyChkAr))
+else:            
+    print(-1)
