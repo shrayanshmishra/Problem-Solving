1a5712e43dc6f252fb738943f7f776233d65abed
Shrayansh Mishra <shreyanshmishra477@gmail.com>
2022-06-02 14:17:36 +0530

    HackerRank Couting Valleys problem
    https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=true

--- CountingValleys.py
+++ CountingValleys.py
@@ -1,8 +1,17 @@
+# given no. of steps taken by the hiker or you can say length of path list is given
 steps = int(input())
+
+# given movement of the hiker as a string containg D or U, whether he takes next step up or down
 path = str(input())
 
+# now to keep count of no. of valleys hiker encounter a variable
 noOfValleys = 0
+
+# notice what the problem says
+# a valley starts when the hiker steps down from sea level and the valleys only ends when he comes up again to sea level
 verticalPos = []
+
+# so basically he'll always start from sea level, we just have to see when he steps up to the sea level again
 vertical = 0
 
 for i in range(steps):
@@ -12,6 +21,8 @@ for i in range(steps):
     else:
         vertical += 1
         verticalPos.append(vertical)
+
+    # this is the condition where we check if it was a valley or not based on whether hiker step up to sea level or not
     if verticalPos[i] == 0 and verticalPos[i - 1] < 0:
         noOfValleys += 1
 
