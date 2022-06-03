9a579ffb7f7b58e70e17cfdccc1117b4dd81ac5a
Shrayansh Mishra <shreyanshmishra477@gmail.com>
2022-06-03 15:00:13 +0530

    HackerRank Divisible Sum Pairs problem
    https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?isFullScreen=true

--- DivisibleSumPairs.py
+++ DivisibleSumPairs.py
@@ -1,14 +1,10 @@
+# a sample test case
 ar = [1, 3, 2, 6, 1, 2]
 n = len(ar)
 k = 3
 c = 0
 for i in range(n):
-    for j in range(1, n):
-        if i >= j:
-            pass
-        else:
-            # print(i, j)
-            if i < j and ((ar[i] + ar[j]) % k == 0):
-                # print(i, j, ar[i], ar[j], ar[i] + ar[j])
-                c += 1
+    for j in range(i + 1, n):
+        if i < j and ((ar[i] + ar[j]) % k == 0):
+            c += 1
 print(c)
