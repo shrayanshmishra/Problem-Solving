4be6b507c341012efc555835a5847d2a9a59568f
Shrayansh Mishra <shreyanshmishra477@gmail.com>
2022-06-03 15:04:17 +0530

    HackerRank Drawing Book problem
    https://www.hackerrank.com/challenges/drawing-book/problem?isFullScreen=true

--- DrawingBook.py
+++ DrawingBook.py
@@ -5,6 +5,6 @@ if noOfPages % 2 == 0:
     noOfPages += 1
 
 minPgTrnFrnt = pageToReach // 2
-minPgTrnBck = (noOfPages - pageToReach) // 2 
+minPgTrnBck = (noOfPages - pageToReach) // 2
 
 print(minPgTrnFrnt, minPgTrnBck, min(minPgTrnFrnt, minPgTrnBck))
