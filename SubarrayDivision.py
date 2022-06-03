95884594da4eb912854479d44e897e0c26e52cd9
Shrayansh Mishra <shreyanshmishra477@gmail.com>
2022-06-03 15:30:19 +0530

    HackerRank Subarray Division problem
    https://www.hackerrank.com/challenges/the-birthday-bar/problem?isFullScreen=true

--- SubarrayDivision.py
+++ SubarrayDivision.py
@@ -2,12 +2,6 @@ s = [1, 2, 1, 3, 2]
 d = 3
 m = 2
 c = 0
-# for i in range(0, len(s)):
-#     for j in range(i + 1, len(s) + 1):
-#         a = s[i: j]
-#         if sum(a) == d and len(a) == m:
-#             c += 1
-# print(c)
 
 for i in range(len(s) - m + 1):
     a = s[i: i + m]
