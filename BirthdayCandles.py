9c5a99d0176b8b17879826024b82c4e7c4b9dfab
Shrayansh Mishra <shreyanshmishra477@gmail.com>
2022-06-03 09:23:14 +0530

    HackerRank Birthday Cake Candles problem
    https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true

--- BirthdayCandles.py
+++ BirthdayCandles.py
@@ -1,6 +1,15 @@
+# given heights of candles
 arr = [3, 2, 1, 3]
+
+# to store the no. of candles having highest height
 c = 0
 for i in arr:
+
+    # using python in-built function to find highest height
+    # and if any candle has height equal to max. height
+    # counter is increemented
     if i == max(arr):
         c += 1
+
+# candles having highest height
 print(c)
