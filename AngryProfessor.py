ac963fd0da1c00f78f0f894d1d47413acc6e6115
Shrayansh Mishra <shreyanshmishra477@gmail.com>
2022-06-03 11:59:32 +0530

    HackerRank Angry Professor
    https://www.hackerrank.com/challenges/angry-professor/problem?isFullScreen=true

---
+++ AngryProfessor.py
@@ -0,0 +1,27 @@
+# given arrival times of all students
+a = [-1, -6, 8, 3, 0, -5]
+
+# no. of students
+n = len(a)
+
+# threshold no. of students
+k = 4
+
+kcount = 9
+
+cancel_class = "YES"
+
+for i in range(n):
+
+    if a[i] <= 0:
+
+        # count no. of students who came on time
+        kcount += 1
+
+        # if students are equal to threshold no. of students then no cancellation of class
+        if kcount == k:
+
+            cancel_class = "NO"
+            break
+
+print(cancel_class)
